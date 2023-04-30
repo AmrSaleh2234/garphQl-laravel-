77de1faf911272c845d71112a99ed9181557e8f5
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-04-30 13:48:46 +0300

    logout and login mutation

---
+++ app/GraphQL/Mutations/login.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Http\Middleware\ValidateSignature;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Nette\Schema\ValidationException;
+
+final class login
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+
+
+        $user = User::where('email', $args['email'])->first();
+        if(!$user || !Hash::check($args['password'],$user->password) ){
+            return "credential un correct ";
+        }
+        return $user->createToken('API TOKEN')->plainTextToken;
+
+    }
+}
